0e14b4e4df3e91ceb56da6febf0dfcb0651524af
Manish Bisht <manish.bisht490@gmail.com>
2017-04-23 20:05:48 +0530

    added footer and website information

--- js/app.js
+++ js/app.js
@@ -1,21 +1,27 @@
 /**
  * Created by manish on 23/4/17.
  */
-$(document).ready(function() {
+$(document).ready(function () {
     $("#url").kendoMaskedTextBox({
         mask: ""
     });
-    kendo.ui.progress($('[kendo-grid]'), false);
+    $("#tabstrip").kendoTabStrip({
+        animation: {
+            open: {
+                effects: "fadeIn"
+            }
+        }
+    });
 });
-var app = angular.module("ngDownloader", [ "kendo.directives" ]);
-app.controller('searchCtrl', function ($scope, $http){
+var app = angular.module("ngDownloader", ["kendo.directives"]);
+app.controller('searchCtrl', function ($scope, $http) {
     $scope.search = function () {
-        if($scope.query){
+        if ($scope.query) {
             console.log($scope.query);
             kendo.ui.progress($("#search"), true);
             $http({
-                method : "GET",
-                url : "https://s1jv2959n8.execute-api.us-east-1.amazonaws.com/v1?q="+$scope.query
+                method: "GET",
+                url: "https://s1jv2959n8.execute-api.us-east-1.amazonaws.com/v1?q=" + $scope.query
             }).success(function mySucces(response) {
                 console.log(response);
                 $scope.data = response;
