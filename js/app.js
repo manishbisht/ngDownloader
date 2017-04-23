b45135d50db281f118ca954bc565a29190ee5c76
Manish Bisht <manish.bisht490@gmail.com>
2017-04-23 16:43:22 +0530

    app.ja added

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/**
+ * Created by manish on 23/4/17.
+ */
+$(document).ready(function() {
+    $("#url").kendoMaskedTextBox({
+        mask: ""
+    });
+    kendo.ui.progress($('[kendo-grid]'), false);
+});
+var app = angular.module("ngDownloader", [ "kendo.directives" ]);
+app.controller('searchCtrl', function ($scope, $http){
+    $scope.search = function () {
+        if($scope.query){
+            console.log($scope.query);
+            $http({
+                method : "GET",
+                url : "https://s1jv2959n8.execute-api.us-east-1.amazonaws.com/v1?q="+$scope.query
+            }).success(function mySucces(response) {
+                console.log(response);
+                $scope.data = response;
+            });
+        }
+    }
+});
